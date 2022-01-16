78cfc3003f39ebc58057c60a4e6051a5fabd0e03
wjp <wjp@hebau.edu>
2022-01-16 20:19:14 +0800

    third commit

--- src/main/java/edu/hebau/git/GitTest.java
+++ src/main/java/edu/hebau/git/GitTest.java
@@ -3,5 +3,6 @@ package edu.hebau.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!   !!");
+        System.out.println("hello git2!   !!");
     }
 }
