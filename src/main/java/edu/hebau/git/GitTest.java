0fe89e7fc57774625635daaabe9a4540da237f72
mrwang15 <89230523+mrwang15@users.noreply.github.com>
2022-01-16 20:57:38 +0800

    pull commit

--- src/main/java/edu/hebau/git/GitTest.java
+++ src/main/java/edu/hebau/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello master  22222!!!!");
         System.out.println("hello hot-fix22222222!!!!");
         System.out.println("hello push test!!!!");
+        System.out.println("hello pull tset!!!");
     }
 }
