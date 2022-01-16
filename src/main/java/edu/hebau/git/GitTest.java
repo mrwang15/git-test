d763c5d38ab1e9b88d5277408ee6da242fd0211a
wjp <wjp@hebau.edu>
2022-01-16 20:51:31 +0800

    push commit

--- src/main/java/edu/hebau/git/GitTest.java
+++ src/main/java/edu/hebau/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello hot-fix!!!!");
         System.out.println("hello master  22222!!!!");
         System.out.println("hello hot-fix22222222!!!!");
+        System.out.println("hello push test!!!!");
     }
 }
