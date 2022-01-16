85059d195bea60c20cd9b927851e68949ac26c5a
wjp <wjp@hebau.edu>
2022-01-16 20:12:19 +0800

    first commit

---
+++ src/main/java/edu/hebau/git/GitTest.java
@@ -0,0 +1,12 @@
+package edu.hebau.git;
+
+/**
+ * @Author 王佳鹏
+ * @Date 2022/1/16 20:08
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
